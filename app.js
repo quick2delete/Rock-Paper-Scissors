4248956b5ddf3f8a0162bd942c6a3d093830fa56
Hae Lee <haeyounglee212@gmail.com>
2022-07-16 14:19:53 -0400

    'UPDATE'

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-// create function that randomly returns rock, paper, scissors
+const btns = document.querySelectorAll(".btn");
+
+const container = document.querySelector("#container");
+// create function that randomly returns rock, paper, scissors by computer
 function computerPlay() {
   // create random number 1, 2, 3
   const num = Math.floor(Math.random() * 3) + 1;
-
   // if statement to get the result
   if (num === 1) {
     return "rock";
@@ -12,55 +14,53 @@ function computerPlay() {
     return "scissors";
   }
 }
-
-// Create variables for playRound function
-// const playerSelection = prompt("Enter Your Choice: ").toLowerCase();
 const computerSelection = computerPlay();
 
+// Function for comparison
 function playRound(playerSelection, computerSelection) {
   // Comparison
   if (playerSelection === "rock") {
     if (computerSelection === "scissors") {
-      console.log("You Won! Rock beats scissors");
+      return "You Won! Rock beats scissors";
     } else if (computerSelection === "paper") {
-      console.log("You Lose! Paper beats Rock");
+      return "You Lose! Paper beats Rock";
     } else {
-      console.log("No winner! Start Again!");
+      return "No winner! Start Again!";
     }
   }
   if (playerSelection === "scissors") {
     if (computerSelection === "rock") {
-      console.log("You Lose! Rock beats scissors");
+      return "You Lose! Rock beats scissors";
     } else if (computerSelection === "paper") {
-      console.log("You Won! Scissors beats paper");
+      return "You Won! Scissors beats paper";
     } else {
-      console.log("No winner! Start Again!");
+      return "No winner! Start Again!";
     }
   }
   if (playerSelection === "paper") {
     if (computerSelection === "rock") {
-      console.log("You Won! Paper beats Rock");
+      return "You Won! Paper beats Rock";
     } else if (computerSelection === "scissors") {
-      console.log("You Lose! Scissors beats paper");
+      return "You Lose! Scissors beats paper";
     } else {
-      console.log("No winner! Start Again!");
+      return "No winner! Start Again!";
     }
   }
-  console.log(`Player: ${playerSelection},  Computer: ${computerSelection}`);
 }
 
-// Invoke playRound function
-// playRound(playerSelection, computerSelection);
+// Event handling with click
+let data = { property: "Data" };
 
-// game function
-
-function game() {
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Enter Your Choice: ").toLowerCase();
-    playRound(playerSelection, computerPlay());
-    // console.log(computerSelection);
-  }
-}
-
-// Calling game function
-console.log(game());
+btns.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    //Reassign property value of data
+    data.property = e.target.textContent.toLowerCase();
+    //Execute function and assign to result variable
+    const result = playRound(data.property, computerSelection);
+    //Create elements and add content on HTML
+    const content = document.createElement("div");
+    content.classList.add("content");
+    content.textContent = result;
+    container.appendChild(content);
+  });
+});
